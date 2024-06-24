df7fcc186ef8e400c81b4c32596a3437d9734373
Kappy <Kapitanov96@yandex.ru>
2024-06-24 19:20:33 +0300

    Поправит переход на точку

--- apps/shop/src/pages/Shops/Shops.tsx
+++ apps/shop/src/pages/Shops/Shops.tsx
@@ -4,28 +4,13 @@ import Header from "./Header";
 import { BackIcon24, Flex, Grid, Page, theme } from "@book-eat/ui";
 import { ShopsContext } from "./context.ts";
 import { useNavigate } from "react-router-dom";
-import { navigateToPage, PageURLS } from "../../constants/urls.ts";
-import { placesEndpoints } from "@book-eat/api";
 import PageHeader from "./PageHeader";
 
 const Shops: FC = () => {
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState("");
-  const { data, isSuccess } = placesEndpoints.useFetchPlacesQuery();
 
   const onBackClick = () => navigate("..");
-
-  useEffect(() => {
-    if (!isSuccess) {
-      return;
-    }
-    if (data.ids.length === 1) {
-      const url = navigateToPage(PageURLS.PRODUCTS, {
-        id: data.entities[0]?.id ?? "test",
-      });
-      navigate(url);
-    }
-  }, [data, isSuccess]);
 
   return (
     <ShopsContext.Provider value={{ searchValue, setSearchValue }}>
