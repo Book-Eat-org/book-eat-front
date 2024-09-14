c5acc8091fb76d4ab208af13580a00f708c1b228
Kappy <Kapitanov96@yandex.ru>
2024-09-14 11:01:47 +0300

    Правочки

--- apps/shop/src/pages/Order/Detail/Submit/Submit.tsx
+++ apps/shop/src/pages/Order/Detail/Submit/Submit.tsx
@@ -1,8 +1,8 @@
-import { useOrder } from "./useOrder.ts";
 import { Button, Flex } from "@book-eat/ui";
 import { useSearchParams } from "react-router-dom";
 import { OrderStatus, ordersEndpoints } from "@book-eat/api";
 import { isNotNil } from "ramda";
+import { useOrder } from "../useOrder.ts";
 
 export const Submit = () => {
   const { id } = useOrder();
