5ff14939e71d4cd1314e4635c6c5378d0bab6c45
Kappy <Kapitanov96@yandex.ru>
2024-10-16 19:35:44 +0300

    Правочки

--- apps/admin/src/pages/Orders/Detail/PageHeader/utils.ts
+++ apps/admin/src/pages/Orders/Detail/PageHeader/utils.ts
@@ -3,6 +3,8 @@ import { OrderStatus } from "$enums";
 
 export const getStatusLabel = cond([
   [equals(OrderStatus.CANCELLED_BY_PROVIDER), always("отменен!")],
+  [equals(OrderStatus.CANCELLED_BY_CLIENT), always("отменен!")],
   [equals(OrderStatus.IN_PROGRESS), always("в работе!")],
+  [equals(OrderStatus.COMPLETED), always("выполнен!")],
   [T, always(undefined)],
 ]);
