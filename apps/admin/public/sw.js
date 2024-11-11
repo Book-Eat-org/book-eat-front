1953c725cfdd948083937eddb4aaa869e3147b15
Kappy <Kapitanov96@yandex.ru>
2024-11-11 15:35:08 +0300

    Правочки

--- apps/admin/public/sw.js
+++ apps/admin/public/sw.js
@@ -1,7 +1,4 @@
 self.addEventListener("message", (event) => {
-  console.log(event.data); // outputs {'hello':'world'}
-
-  console.log(Notification.permission);
   if (Notification.permission === "granted") {
     self.registration.showNotification(event.data);
   }
