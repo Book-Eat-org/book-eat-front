cf942d2f7a2d537d7da9d3dded1b5815cdb223dd
Kappy <Kapitanov96@yandex.ru>
2024-11-17 21:25:38 +0300

    Правочки

--- apps/shop/src/pages/Products/Products.tsx
+++ apps/shop/src/pages/Products/Products.tsx
@@ -1,10 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { Items } from "./Items";
-import { Card } from "./Card";
 
 export const Products = () => (
   <Routes>
-    <Route path="detail/:id" element={<Card />} />
     <Route path=":id" element={<Items />} />
   </Routes>
 );
