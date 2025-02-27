29a26dd5e1459a390ca775cac4168bf5fde7816f
Kiva Vlad <melnilovv717@gmail.com>
2025-02-28 00:27:04 +0300

    Проверка уникальность категорий при создании

--- apps/admin/src/pages/Menu/Groups/AddItem/AddItem.tsx
+++ apps/admin/src/pages/Menu/Groups/AddItem/AddItem.tsx
@@ -1,21 +1,63 @@
-import { FC, useState } from "react";
-
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import { useController, FormProvider, useForm } from "react-hook-form";
 import { BackIcon24, Button, Flex, theme, UIGrid, UIInput } from "@book-eat/ui";
 import { categoriesEndpoints } from "$api";
+import { categoriesSelectors } from "$store";
 import { Page } from "$components";
 import { useNavigate } from "react-router-dom";
+import { IFormState } from "./models";
 
 const AddItem: FC = () => {
   const navigate = useNavigate();
-
-  const [title, setTitle] = useState("");
   const [trigger] = categoriesEndpoints.useCreateCategoryMutation();
+
+  const data = useSelector(categoriesSelectors.selectAll);
+  const titles = data?.flat().map(item => item.title?.toLowerCase()) ?? [];
+
+  const defaultValues: IFormState = {
+    category: '',
+    confirmation: false,
+  };
+
+  const methods = useForm<IFormState>({
+    defaultValues,
+  });
+
+  const { field, fieldState } = useController<IFormState, "category">({
+    name: "category",
+    control: methods.control,
+    rules: { 
+      required: { 
+        value: true, 
+        message: "Поле не может быть пустым. Пожалуйста, придумайте название." 
+      },
+      validate: {
+        uniqueTitle: (value) => {
+          if (value) {
+            const categoryValue = value.toLowerCase().trim();
+            if (titles.includes(categoryValue)) {
+              return "Такая категория уже существует. Пожалуйста, придумайте новое название.";
+            }
+            return true;
+          }
+        }
+      }
+    },
+  });
+
+  const { onChange, value } = field;
+  const { error } = fieldState;
 
   const navigateBack = () => navigate("..");
 
-  const onSubmit = async () => {
-    await trigger({ title, isActive: true });
-    navigateBack();
+  const onSubmit = async (data: IFormState) => {
+    const { category } = data;
+    
+    if (category?.trim()) {
+      await trigger({ title: category, isActive: true });
+      navigateBack();
+    }
   };
 
   return (
@@ -33,17 +75,22 @@ const AddItem: FC = () => {
         <Page.Header.Title>Категории</Page.Header.Title>
       </Page.Header>
       <Page.Body>
-        <UIGrid gap="30px" padding="0 0 20px">
-          <UIInput
-            placeholder="Название категории"
-            value={title}
-            onChange={setTitle}
-          />
-          <UIGrid colSizes="1fr 2fr" gap="64px">
-            <Button onClick={navigateBack}>Отменить</Button>
-            <Button onClick={onSubmit}>Добавить категорию</Button>
-          </UIGrid>
-        </UIGrid>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <UIGrid gap="30px" padding="0 0 20px">
+              <UIInput
+                placeholder="Название категории"
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+              />
+              <UIGrid colSizes="1fr 2fr" gap="64px">
+                <Button onClick={navigateBack}>Отменить</Button>
+                <Button type="submit">Добавить категорию</Button>
+              </UIGrid>
+            </UIGrid>
+          </form>
+        </FormProvider>
       </Page.Body>
     </Page>
   );
